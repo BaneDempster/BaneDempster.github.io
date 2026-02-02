49d41bc7704bbb65c99ff10fa732c27e09749be8
BaneDempster <dempst546@wearescpps.org>
2026-02-02 14:20:23 +0000

    finished dice app project

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -5,7 +5,7 @@ $("<div>")
   .css("height", 15)
   .css("width", 15)
   .css("background-color", "black")
-  .css("position", "fixed")
+  .css("position", "absolute")
   .css("top", top)
   .css("left", left)
   .css("border-radius", "25px")
@@ -15,32 +15,32 @@ function rollDie(dieID){
   $(dieID).empty();
   var randomNum = Math.ceil(Math.random() * 6);
   if (randomNum === 1) {
-  makeDot(50, 50, dieID); // middle middle
+  makeDot(45, 40, dieID); // middle middle
 } else if (randomNum === 2) {
-  makeDot(25, 25, dieID); // top left
+  makeDot(10, 10, dieID); // top left
   makeDot(75, 75, dieID); // bottom right
 } else if (randomNum === 3) {
-  makeDot(25, 25, dieID); // top left
+  makeDot(10, 10, dieID); // top left
   makeDot(75, 75, dieID); // bottom right
-  makeDot(50, 50, dieID); // middle middle
+  makeDot(45, 40, dieID); // middle middle
 } else if (randomNum === 4) {
   makeDot(75, 75, dieID); // bottom right
-  makeDot(25, 25, dieID); // top left
-  makeDot(25, 75, dieID); // bottom left
-  makeDot(75, 25, dieID); // top right
+  makeDot(10, 10, dieID); // top left
+  makeDot(10, 75, dieID); // bottom left
+  makeDot(75, 10, dieID); // top right
 } else if (randomNum === 5) {
-  makeDot(50, 50, dieID); // middle middle
+  makeDot(45, 40, dieID); // middle middle
   makeDot(75, 75, dieID); // bottom right
-  makeDot(25, 25, dieID); // top left
-  makeDot(25, 75, dieID); // bottom left
-  makeDot(75, 25, dieID); // top right
+  makeDot(10, 10, dieID); // top left
+  makeDot(10, 75, dieID); // bottom left
+  makeDot(75, 10, dieID); // top right
 } else if (randomNum === 6){
   makeDot(75, 75, dieID); // bottom right
-  makeDot(25, 25, dieID); // top left
-  makeDot(25, 75, dieID); // bottom left
-  makeDot(75, 25, dieID); // top right
-  makeDot(50, 25, dieID); // middle left
-  makeDot(50, 75, dieID); // middle right
+  makeDot(10, 10, dieID); // top left
+  makeDot(10, 75, dieID); // bottom left
+  makeDot(75, 10, dieID); // top right
+  makeDot(45, 10, dieID); // middle left
+  makeDot(45, 75, dieID); // middle right
 }
   console.log(randomNum);
 }
@@ -48,9 +48,7 @@ function handleClick(){
   rollDie("#die");
   rollDie("#die2");
 }
-$("#die").on("click", handleClick);
-function handleClick2(){
-  rollDie("#die2");
-}
-$("#die2").on("click", handleClick2);
+$("button")
+.text("Roll Dice")
+.on("click", handleClick);
 });
