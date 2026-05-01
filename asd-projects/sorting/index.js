2466c05a529978dc07821f3c26e27e6a7db08556
BaneDempster <dempst546@wearescpps.org>
2026-05-01 12:46:47 +0000

    finished sorting

--- asd-projects/sorting/index.js
+++ asd-projects/sorting/index.js
@@ -31,15 +31,15 @@ async function bubbleSort(array){
 }
 
 // TODO 3: Implement quickSort
-async function quicksort(array, left, right){
+async function quickSort(array, left, right){
     if((right - left) > 0){
         var index = await partition(array, left, right);
         
         if(left < (index - 1)){
-        await quicksort(array, left, index - 1);
+        await quickSort(array, left, index - 1);
     }
         if(right > index){
-        await quicksort(array, index, right);
+        await quickSort(array, index, right);
     }
     }
 }
